0cb02533008fa7b8e0fc6300915ce586bb90f9ce
Sara Sabbagh <sarasabbagh@hotmail.co.uk>
2022-09-05 17:26:14 +0100

    worked on mapBox api ideas

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -10,12 +10,42 @@ import { Mission } from './components/pages/Mission';
 import { AppFunctionality } from './components/pages/AppFunctionality';
 import { NewJourneyPage } from './components/pages/NewJourneyPage';
 
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl';
+mapboxgl.accessToken = 'pk.eyJ1IjoicG1vbnNvbjEiLCJhIjoiY2w3b2x5eGd1MDhldjN1bzc2bHdrZWt4aCJ9.j1NGCKtR_vtfwz9JdVGi3A';
+
 import './App.css';
-import { NewJourney } from './components/pages/NewJourneyPage';
+
 
 function App() {
+
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
+
+
+
   return (
+  
+      
+
     <div className= "App">
+      <div>
+        <div ref={mapContainer} className="map-container" />
+      </div>
     <BrowserRouter>
     <Routes>
           <Route path='/' element={<IndexPage />} />
